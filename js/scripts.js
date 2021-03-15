d5b770d1e252226af225819656acbed6d63c5994
cnjlv <79581768+cnjlv@users.noreply.github.com>
2021-03-15 12:14:09 +0100

    general changes

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+alert('Hello world');
+
+let myFavoriteFood = 'Pasta';
+document.write(myFavoriteFood);
+
+myFavoriteFood = 'Sushi';
+document.write(myFavoriteFood);
+
+let myFavoriteSeason = 'Spring';
+document.write(myFavoriteSeason);
+
